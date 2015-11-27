0af0afb6f3060e1b5133e3dbb23f1d705a2c8413
vagelim <evan@datadoghq.com>
2015-11-27 15:58:13 -0500

    new options
    added new options to support multi-user usage

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 # __author__ = 'vagelim'
 #Authentication and configuration
 
-
-AUTHORIZED_NUMBERS = ['', ''] #Authorized phone numbers
-DEFAULT_NUMBER = '' #Number to send debug messages to 
-NEXMO_NUMBER = ''
+DEFAULT_NUMBER = '12120000000' #Number to send debug messages to 
+NEXMO_NUMBER = '12240000000'
 
 
 NEXMO_API_KEY  = '' 
 NEXMO_SECRET = '' 
 
 #GSPREAD
-SHEETS_URL = 'https://docs.google.com/spreadsheets/d/really-long-UUID/edit#gid=0'
+SHARE_EMAIL = 'account-398@gentle-corpuscle-113209.iam.gserviceaccount.com' #client_email in gspread OAUth2 json file
+ADMIN_BOOK = 'https://docs.google.com/spreadsheets/d/administrative-workbook/edit #Book for administration
+GCONF = 'goo.json'
+#SHEETS_URL MUST end with /edit
 
-GCONF = ''
-#Remember, SHEETS_URL must end with "edit#gid=0" and MUST be shared with "client_email" from json file
-#In our case, must be shared with: account-1@conspicuous-interface-113300.iam.gserviceaccount.com
 
+#Allow users to add themselves via txt message
+SELF_SERVICE = True
 
 #Control characters and keywords for the command interpreter are in interpreter.py
