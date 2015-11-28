291b27712c562147beb98d0fd65fe5995308d177
vagelim <evan@datadoghq.com>
2015-11-28 15:37:57 -0500

    flake8
    removed unused things
    normalized style somewhat
    included checking for invalid commands
    
    Revert "Revert "flake8""
    
    This reverts commit 52e79e9ad72c37c3fbd0453c647ad59729fa4953.

--- gspread_utils.py
+++ gspread_utils.py
@@ -4,13 +4,15 @@ import gspread
 from oauth2client.client import SignedJwtAssertionCredentials
 
 from config import GCONF, ADMIN_BOOK, SELF_SERVICE, SHARE_EMAIL
-from sms import sendTxt #Be able to send SMS
+from sms import sendTxt  # Be able to send SMS
+
 
 def getWorkbook(url):
     json_key = json.load(open(GCONF))
     scope = ['https://spreadsheets.google.com/feeds']
 
-    credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
+    credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key[
+                                                'private_key'].encode(), scope)
 
     gc = gspread.authorize(credentials)
 
@@ -18,130 +20,130 @@ def getWorkbook(url):
 
     return book
 
+
 def getSheet(sheet, book):
     """Takes a sheet name and book, returns the sheet"""
 
-
     list_sheets = book.worksheets()
-    #Check to see if the sheet exists
+    # Check to see if the sheet exists
     if sheet in str(list_sheets):
-        #It does, use it
+        # It does, use it
         sheet = book.worksheet(sheet)
 
-    #If not, create and use it
+    # If not, create and use it
     else:
         sheet = book.add_worksheet(title=sheet, rows="100", cols="20")
-        #Create the 'magic cell'
-        #The magic cell is the total of all expenses
+        # Create the 'magic cell'
+        # The magic cell is the total of all expenses
 
         sheet.update_acell('D1', '=SUM(B1:B100)')
 
     return sheet
 
-def getValue(cell, sheet, book=None ):
+
+def getValue(cell, sheet, book=None):
     pass
+
 
 def commit(content, sheet):
     """Takes a message object and writes expenses to a spreadsheet"""
-    #Get the date of the message, to determine which spreadsheet to use
-    date = content['timestamp']
 
-    #Unpack the content
-    #Expenses follow the format: <NAME>:cost with spaces to separate multiple values
-    #Example: Food:10 drink:20 movie:15
+    # Unpack the content
+    # Expenses follow the format: <NAME>:cost with spaces to separate multiple values
+    # Example: Food:10 drink:20 movie:15
 
-    col_values = sheet.col_values(1) #First column lists item, adjacent column lists price
-    costs = content['message'].split(' ') 
-    number = len(costs)
+    col_values = sheet.col_values(1)  # First column lists item, adjacent column lists price
+    costs = content['message'].split(' ')
 
-
-    for each in costs: # NAME IS CASESENSITIVE
-        name, price = each.split(':')
-
-        #Check if the item exists already
-        if name in col_values: #If it does, add to that
+    for each in costs:  # all text is lowercase
+        try:  # Check if the message is malformed
+            name, price = each.split(':')
+        except ValueError:
+            return "malformed"
+        # Check if the item exists already
+        if name in col_values:  # If it does, add to that
             cell = sheet.find(name)
             new_col = cell.col + 1
 
             current_val = sheet.cell(cell.row, new_col).numeric_value
 
-            if current_val == None:
+            if current_val is None:
                 current_val = float(price)
 
             else:
                 current_val += float(price)
 
-            sheet.update_cell(cell.row, new_col, current_val) 
-            #TODO check updated value
+            sheet.update_cell(cell.row, new_col, current_val)
+            # TODO check updated value
 
-        #The item does not exist
+        # The item does not exist
         else:
-            #Find the cardinality of the last element in col
-            depth  = len(sheet.col_values(1))
+            # Find the cardinality of the last element in col
+            depth = len(sheet.col_values(1))
             depth += 1
-            sheet.update_cell(depth, 1 , name) #Append name to the last row, first column
-            sheet.update_cell(depth, 2 , price) #Append price to the last row, second column
+            sheet.update_cell(depth, 1, name)  # Append name to the last row, first column
+            sheet.update_cell(depth, 2, price)  # Append price to the last row, second column
+
 
 def getUserBook(phone_number):
     """Takes a phone number, returns phone number's workbook"""
-    #Get Admin workbook
+    # Get Admin workbook
     book = getWorkbook(ADMIN_BOOK)
 
-    #Get user sheet
+    # Get user sheet
     sheet = getSheet("users", book)
 
-    #Check if phone_number is a valid user
-    col_values = sheet.col_values(1) #First column lists users, second column is workbook URL
+    # Check if phone_number is a valid user
+    col_values = sheet.col_values(1)  # First column lists users, second column is workbook URL
     if phone_number in col_values:
         cell = sheet.find(phone_number)
-        #URL is adjacent column
+        # URL is adjacent column
         URL = sheet.cell(cell.row, cell.col + 1).value
 
-        if URL[:4] == 'http': #If 'valid' URL
+        if URL[:4] == 'http':  # If 'valid' URL
             try:
                 book = getWorkbook(URL)
 
-            except gspread.exceptions.SpreadsheetNotFound: #Workbook is not shared
+            except gspread.exceptions.SpreadsheetNotFound:  # Workbook is not shared
                 sendTxt('Workbook not found. Make sure you shared it with: ' + SHARE_EMAIL, phone_number)
                 return -1
-            except gspread.exceptions.NoValidUrlKeyFound: #Workbook URL is invalid
+            except gspread.exceptions.NoValidUrlKeyFound:  # Workbook URL is invalid
+                sendTxt("Workbook is invalid. Please reply with a valid Google Sheets URL.", phone_number)
                 return -1
-                addUser(phone_number, URL='invalid')
-            
+
             return book
 
-        elif URL == '': #Field is empty
+        elif URL == '':  # Field is empty
             pass
 
-    else: #If not an existing user
+    else:  # If not an existing user
         addUser(phone_number)
         return -1
 
-def addUser(phone_number , URL=None):
 
-    if SELF_SERVICE == False: #Users must be manually added
+def addUser(phone_number, URL=None):
+
+    if SELF_SERVICE is False:  # Users must be manually added
         return -1
 
-    if URL == None:
+    if URL is None:
         sendTxt("User not found. Please reply with a Google Sheets URL.", phone_number)
 
-    if URL == 'invalid': #URL was invalid, user needs to make a new one
-        sendTxt("Workbook is invalid. Please reply with a valid Google Sheets URL.", phone_number)
-    #Get Admin workbook
+    # Get Admin workbook
     book = getWorkbook(ADMIN_BOOK)
 
-    #Get user sheet
+    # Get user sheet
     sheet = getSheet("users", book)
 
-    #Check if phone number has tried to add itself already
-    col_values = sheet.col_values(1) #First column lists users, second column is workbook URL
+    # Check if phone number has tried to add itself already
+    col_values = sheet.col_values(1)  # First column lists users, second column is workbook URL
     if phone_number in col_values:
         cell = sheet.find(phone_number)
-        #Update the URL of the user's book
+        # Update the URL of the user's book
         sheet.update_cell(cell.row, cell.col + 1, URL)
 
-    else: #The user just got sent a message telling them to add the URL
-        #Add them to the db, so they will be recognized when they respond
+    else:  # The user just got sent a message telling them to add the URL
+        # Add them to the db, so they will be recognized when they respond
         depth = len(sheet.col_values(1)) + 1
         sheet.update_cell(depth, 1, phone_number)
-
+        
--- interpreter.py
+++ interpreter.py
@@ -1,65 +1,73 @@
 #!/usr/bin/env python
 # __author__ = 'vagelim'
 
-import gspread_utils as gs#Testing
+import gspread_utils as gs  # Testing
 from config import DEFAULT_NUMBER, SHARE_EMAIL
 from sms import sendTxt
 
 ############
 # KEYWORDS #
 ############
-ADMIN = '!' #Admin command prefix
-EXPENSE = '@' #For expense tracking
-NEW_USER = 'add' #New user keyword
+ADMIN = '!'  # Admin command prefix
+EXPENSE = '@'  # For expense tracking
+NEW_USER = 'add'  # New user keyword
+
 
 def interpreter(content=None):
     """Takes a message object, containing: msisdn, recipient, message, id, timestamp"""
-    if content == None or content['message'] == '':
+    if content is None or content['message'] == '':
         return 0
 
-    print content['message'] #Debugging
+    print content['message']  # Debugging
 
-    #Add 1 to the front of the phone number (Nexmo requires it for sending SMS)
-    #Makes this app USA only for the moment :(
+    # Add 1 to the front of the phone number (Nexmo requires it for sending SMS)
+    # Makes this app USA only for the moment :(
     if content['msisdn'][0] != '1':
-        #Append it
+        # Append it
         l = []
         l.append('1')
         l.append(content['msisdn'])
         content['msisdn'] = ''.join(l)
 
-    #Message contains administrative commands
+    # Message contains administrative commands
     if ADMIN is content['message'][0]:
         pass
 
-    #If it is a request from a user to add themselves
+    # If it is a request from a user to add themselves
     if NEW_USER == content['message'].lower():
         gs.addUser(content['msisdn'])
 
-    #Message contains expenses
+    # Message contains expenses
     elif EXPENSE is content['message'][0]:
-        #Clean the timestamp
+        # Clean the timestamp
         content['timestamp'] = content['timestamp'].split()[0]
-        #Clean and normalize the message
+        # Clean and normalize the message
         content['message'] = content['message'][1:].lower()
 
-        #Get user workbook
+        # Get user workbook
         book = gs.getUserBook(content['msisdn'])
 
-        if book != -1: #Book == -1 if user does not exist
+        if book != -1:  # Book == -1 if user does not exist
             sheet = gs.getSheet(content['timestamp'], book)
-            gs.commit(content, sheet)
+            result = gs.commit(content, sheet)
+            if 'malformed' == result:
+                sendTxt(
+                    "New users, txt: 'add'\nAdd expense format:\n@<category>:cost <category>:cost", content['msisdn'])
 
-    elif 'http' == content['message'][:4]: #If the message is a URL, assume the user is trying to add themselves to the system
+    # If the message is a URL, assume the user is trying to add themselves
+    # to the system. This also allows for updating one's workbook on the fly.
+    elif 'http' == content['message'][:4]:
         gs.addUser(content['msisdn'], content['message'])
         sendTxt("Workbook added!\n Make sure you share it with " + SHARE_EMAIL, content['msisdn'])
 
-if __name__ == '__main__': #For testing, should never run as main()
+if __name__ == '__main__':  # For testing, should never run as main()
     import sys
     try:
         interpreter(sys.argv[1])
     except IndexError:
-        content = {'msisdn' : DEFAULT_NUMBER, 'message' : "@Food:10", 'recipient': DEFAULT_NUMBER, 'timestamp' : '2015-11-23 none'}
+        content = {'msisdn': DEFAULT_NUMBER, 'message': "@Food:10",
+                   'recipient': DEFAULT_NUMBER, 'timestamp': '2015-11-23 none'}
         content['message'] = raw_input("Command to run: ")
         interpreter(content)
     exit()
+    
